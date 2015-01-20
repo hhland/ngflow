6243c5172c99e9bb6bcde49c343dc30a9bb51e60
hhlin <smartken0824@gmail.com>
2015-01-20 14:03:35 +0800

    add outer project

---
+++ SMSServices/Modem.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Data;
+using System.Runtime.InteropServices; 
+
+namespace BP
+{
+    public enum GSMSta
+    {
+        /// <summary>
+        ///  Stagnate 
+        /// </summary>
+        Stop,
+        /// <summary>
+        ///  Run 
+        /// </summary>
+        Runing
+    }
+    public class Modem
+    {
+
+
+        #region API
+        /// <summary>
+        ///  Main entry point for the application .
+        /// </summary>
+        [STAThread]
+        [DllImport("sms.dll", EntryPoint = "Sms_Connection")]
+        public static extern uint Sms_Connection(string CopyRight, uint Com_Port, uint Com_BaudRate, out string Mobile_Type, out string CopyRightToCOM);
+
+        [DllImport("sms.dll", EntryPoint = "Sms_Disconnection")]
+        public static extern uint Sms_Disconnection();
+
+        [DllImport("sms.dll", EntryPoint = "Sms_Send")]
+        public static extern uint Sms_Send(string Sms_TelNum, string Sms_Text);
+
+        [DllImport("sms.dll", EntryPoint = "Sms_Receive")]
+        public static extern uint Sms_Receive(string Sms_Type, out string Sms_Text);
+
+        [DllImport("sms.dll", EntryPoint = "Sms_Delete")]
+        public static extern uint Sms_Delete(string Sms_Index);
+
+        [DllImport("sms.dll", EntryPoint = "Sms_AutoFlag")]
+        public static extern uint Sms_AutoFlag();
+
+        [DllImport("sms.dll", EntryPoint = "Sms_NewFlag")]
+        public static extern uint Sms_NewFlag(); 
+        #endregion
+        /// <summary>
+        ///  Status 
+        /// </summary>
+        public static GSMSta GSMState = GSMSta.Stop;
+        /// <summary>
+        ///  Connected to the device 
+        /// </summary>
+        /// <returns></returns>
+        public static bool Conn()
+        {
+            try
+            {
+                String TypeStr = "";
+                String CopyRightToCOM = "";
+                String CopyRightStr = "// Shanghai Information Technology Co., fast race , Site www.xunsai.com//";
+                for (int i = 0; i < 10; i++)
+                {
+                    if (Sms_Connection(CopyRightStr, uint.Parse(i.ToString()), 9600, out TypeStr, out CopyRightToCOM) == 1)
+                    {
+                        ///5 For the serial number ,0 Infrared interface ,1,2,3,... For the serial 
+                        GSMState = GSMSta.Runing;
+                        Console.Beep();
+                        return true;
+                    }
+                    // Stop 5秒.
+                    System.Threading.Thread.Sleep(5000);
+                }
+                GSMState = GSMSta.Stop;
+                return false;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(" An error occurred when connecting devices , Please check plugged .\t\n Technical Information :"+ex.Message, 
+                    " Prompt ", MessageBoxButtons.OK);
+                return false;
+            }
+        }
+        /// <summary>
+        ///  Broken Links 
+        /// </summary>
+        /// <returns></returns>
+        public static void Close()
+        {
+            Sms_Disconnection();
+        }
+        /// <summary>
+        ///  Performing transmission 
+        /// </summary>
+        /// <param name="tel"></param>
+        /// <param name="msg"></param>
+        /// <returns></returns>
+        public static bool Send(string tel, string msg)
+        {
+            if (Sms_Send(tel, msg) == 1)
+                return true;
+            else
+                return false;
+        }
+        public Modem()
+        {
+        }
+    }
+}
---
+++ WorkFlowTrack/App.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace WorkFlowTrack
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            this.Startup += this.Application_Startup;
+            this.Exit += this.Application_Exit;
+            this.UnhandledException += this.Application_UnhandledException;
+
+            InitializeComponent();
+        }
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            this.RootVisual = new MainPage();
+        }
+
+        private void Application_Exit(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Application_UnhandledException(object sender, ApplicationUnhandledExceptionEventArgs e)
+        {
+            //  If the application is running outside the debugger , Then use the browser 
+            //  Exception reporting mechanism that exception .在 IE 上, A status bar will be used  
+            //  Yellow alert icon to display the exception ,而 Firefox  Appears a script error .
+            if (!System.Diagnostics.Debugger.IsAttached)
+            {
+
+                //  Watch out :  This allows applications has been thrown in but not yet handle the exception of the case 
+                //  Continue to run . 
+                //  For production applications , This error handling should be replaced with the website report errors 
+                //  And stop the application .
+                e.Handled = true;
+                Deployment.Current.Dispatcher.BeginInvoke(delegate { ReportErrorToDOM(e); });
+            }
+        }
+
+        private void ReportErrorToDOM(ApplicationUnhandledExceptionEventArgs e)
+        {
+            try
+            {
+                string errorMsg = e.ExceptionObject.Message + e.ExceptionObject.StackTrace;
+                errorMsg = errorMsg.Replace('"', '\'').Replace("\r\n", @"\n");
+
+                System.Windows.Browser.HtmlPage.Window.Eval("throw new Error(\"Unhandled Error in Silverlight Application " + errorMsg + "\");");
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
